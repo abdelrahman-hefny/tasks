222bfdb7bd679129b25fc2bf29a2dd2ffe98a18c
abderhman hefny.com <abdohfny21002@gmail>
2024-03-07 20:49:20 +0200

    D

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ const sass = require('gulp-sass')(require('sass'));
 
 // دالة لتحويل Sass إلى CSS
 function compileSass() {
-    return src('./Task Design/sass/style.scss')
+    return src('./kudzoka/sass/style.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(dest('./Task Design'));
+        .pipe(dest('./kudzoka'));
 }
 
 // دالة لمراقبة تغييرات الملفات وتشغيل مهمة التحويل تلقائيًا
 function watchFiles() {
-    watch('./Task Design/sass', compileSass);
+    watch('./kudzoka/sass', compileSass);
 }
 
 // تصدير المهام ليتم تشغيلها باستخدام Gulp
